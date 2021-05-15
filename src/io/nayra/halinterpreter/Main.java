60c7741633af24cae88bff2f14267264ffd4b2a9
pinkfloyd <davidreyesmilian91@gmail.com>
2021-05-15 16:21:40 +0200

    refactor Main.java

--- src/io/nayra/halinterpreter/Main.java
+++ src/io/nayra/halinterpreter/Main.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.File;
-//import java.util.Scanner;
 
 public class Main {
     static float[] memory = new float[100];
-    static float[] registers = new float[16];//static makes string[] accessible from everywhere
+    static float[] registers = new float[16]; //static makes string[] accessible from everywhere
     static float accu = 0;
     static int pc = 0;
     static float io0 = 0;
@@ -21,106 +20,55 @@ public class Main {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-
-        /*System.out.println("Soll der Debug Mode aktiviert werden?");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-
-        if(input == "Yes"){
-            //debugMode = true;
-            
-        }
-        else{
-            //debugMode = false;
-            System.out.println("False");
-        }*/
-        
-        
-        /*char argument;
-        String file;
-        for(int i = 0; i < args.length; i++){
-            
-            argument = args[i].charAt(0);
-            if(args[i].equals("d")){
-                System.out.println("Debug");
-                debugMode = true;
-            }
-            String filename;
-            if(argument == '.'){
-                System.out.println("Pfad");
-                filename = args[i];
-            }
-            try{
-                file = filename;
-            }catch (Exception e) {
-                e.printStackTrace();
-
-            }
-            
-
-        }*/
+        fillArrayWith(memory, 0);
+        fillArrayWith(registers, 0);
 
         debugMode = Arrays.asList(args).contains("-d");
-        System.out.println("Debug enabled: " + debugMode)
-        Arrays.fill(memory, 5);//initializes Elements of registry with 0
-        memory[2] = 3;
+        System.out.println("Debug enabled: " + debugMode);
 
-        Arrays.fill(registers, 0);//initializes Elements of registry with 0
-        //registers[1] = 2;//for debugging
-        //registers[2] = 2;
-
-
-        for (String a : args) {
-            if (new File(a).exists()) {
-                ArrayList<String> script = fileToArrayList(a);
-                interpret(script);
-                break;
-            }
+        String scriptPath = findScriptPath(args);
+        if (scriptPath != null) {
+            ArrayList<String> scriptLines = extractLinesFromFile(scriptPath);
+            interpret(scriptLines);
+        } else {
+            System.out.println("No valid script file found in args");
         }
-
 
         long endTime = System.currentTimeMillis();
 
         System.out.println("That took " + (endTime - startTime) + " milliseconds");
-        //sc.close();
     }
 
-    static ArrayList<String> fileToArrayList(String filename) {
-        ArrayList<String> result = new ArrayList<>();
+    private static String findScriptPath(String[] args) {
+        for (String a : args) {
+            if (new File(a).exists()) {
+                return a;
+            }
+        }
+        return null;
+    }
 
+    /**
+     * initializes all the elements of array with value
+     *
+     * @param array array to be filled with the provided value
+     * @param value used to fill the array
+     */
+    private static void fillArrayWith(float[] array, int value) {
+        Arrays.fill(array, value);
+    }
+
+    static ArrayList<String> extractLinesFromFile(String filename) {
+        ArrayList<String> result = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             while (br.ready()) {
                 result.add(br.readLine());
             }
         } catch (IOException e) {
-
             e.printStackTrace();
         }
         return result;
     }
-
-    /*static void interpret(String program) {
-        String[] instructions = program.split("\n");
-        for (String instruction : instructions) {
-            Instruction o = parseInstruction(instruction);
-            System.out.println(o);
-            if (o != null) o.run();
-        }
-    }*/
-
-    /*static void interpret(ArrayList<String> script) {
-        for (String instruction : script) {
-            Instruction o = parseInstruction(instruction);
-            if (o instanceof StopInstruction) break;
-            Class c = o.getClass();
-            if (debugMode) printForDebug(c);
-            if (o.getInstructionIndex() == pc) {
-                //System.out.println("Compare worked!!");
-                if (o != null) o.run();
-            }
-            if (debugMode) printForDebug(c);
-        }
-    }*/
 
     static void interpret(ArrayList<String> script) {
         while (pc < script.size()) {
@@ -136,16 +84,6 @@ public class Main {
         }
 
     }
-   /* static boolean classUsesRegister(Instruction o) {
-        Class c = o.getClass();
-        if (c == AddInstruction.class) return true;
-        else if (c == SubInstruction.class) return true;
-        else if (c == MultiplicationInstruction.class) return true;
-        else if (c == DivideInstruction.class) return true;
-        else if (c == LoadInstruction.class) return true;
-            //else if (c == StoreInDInstruction.class) return true;
-        else return false;
-    }*/
 
     private static void printForDebug(Class c) {
 
@@ -155,7 +93,6 @@ public class Main {
         }
         System.out.println("PC: " + pc);
         System.out.println("----------------------------------------");
-
     }
 
     static Instruction parseInstruction(String instruction) {
